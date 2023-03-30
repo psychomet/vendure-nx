73c28ddb7e0d966bf7f5bbbc52cfe2796bc2efe5
psychomet <sanatkar.dev@gmail.com>
2023-03-30 23:19:32 +0330

    wip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,12 @@ module.exports = {
     {
       name: 'vendure_server',
       script: './dist/apps/server/main.js',
+      node_args: '-r dotenv/config',
     },
     {
       name: 'vendure_worker',
       script: './dist/apps/worker/main.js',
+      node_args: '-r dotenv/config',
     },
   ],
 };
